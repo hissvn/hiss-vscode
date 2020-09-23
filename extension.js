df5883407f0ce80da43d0fbbb3aaf1c48397a309
Nat Quayle Nelson <natquaylenelson@gmail.com>
2020-09-22 21:19:17 -0600

    Error messages and ready message

--- extension.js
+++ extension.js
@@ -1,99 +1,130 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
+const commands = vscode.commands;
+const infoMessage = vscode.window.showInformationMessage;
+const errorMessage = vscode.window.showErrorMessage;
+const inputBox = vscode.window.showInputBox;
 
 const hiss = require('./hiss-node.js').hiss;
 const HT = hiss.HissTools;
-const hval = hiss.HissTools.toHValue;
-const string = hiss.HissTools.toHaxeString;
-const first = hiss.HissTools.first;
+const hval = HT.toHValue;
+const string = HT.toHaxeString;
+const first = HT.first;
+const value = HT.value;
 
 var interp = null;
 
 function freshInterpreter() {
-	interp = new hiss.CCInterp(
-		// (print) via message box
-		function (v) {
-			vscode.window.showInformationMessage(HT.toPrint(hval(v))); return v;
-		});
-
-	// (read-line) via input box
-	interp.importCCFunction(function(_, _, cc) {
-		vscode.window.showInputBox().then(function(input) {
-			cc(hval(input));
-		}, function (err) {
-			vscode.window.showInformationMessage("read-line failed with " + err.toString());
-		});
-	}, "read-line");
-
-	// (input-string [prompt]) via input box
-	interp.importCCFunction(function(args, _, cc) {
-		var prompt = "";
-		if (hiss.HissTools.truthy(args)) {
-			prompt = string(hiss.HissTools.first(args));
-		}
-		vscode.window.showInputBox({
-			"prompt": prompt
-		}).then(function(input) {
-			cc(hval(input));
-		}, function (err) {
-			vscode.window.showInformationMessage("input-string failed with " + err.toString());
-		});
-	}, "input-string");
-
-	// (insert [expression])
-	interp.importCCFunction(function(args, env, cc) {
-		var arg = hiss.HissTools.first(args);
-		var toInsert = hiss.HissTools.toMessage(arg);
-		if (vscode.window.activeTextEditor) {
-			vscode.window.activeTextEditor.edit(function(edit) {
-				edit.replace(vscode.window.activeTextEditor.selection, toInsert);
-				cc(hval(arg));
+	return new Promise( (resolve, reject) => {
+		interp = new hiss.CCInterp(
+			// (print) via message box
+			(v) => {
+				infoMessage(HT.toPrint(hval(v))); return v;
 			});
-		} else {
-			vscode.window.showInformationMessage("You need to select an editor before inserting.");
+
+		// Handle errors by showing an error message box
+		interp.setErrorHandler((error) => {
+			errorMessage(error);
+		});
+
+		// (read-line) via input box
+		interp.importCCFunction((args, env, cc) => {
+			inputBox().then((input) => {
+				cc(hval(input));
+			}, (err) => {
+				errorMessage("read-line failed with " + err.toString());
+			});
+		}, "read-line");
+
+		// (input-string [prompt]) via input box
+		interp.importCCFunction((args, env, cc) => {
+			var prompt = "";
+			if (HT.truthy(args)) {
+				prompt = string(first(args));
+			}
+			inputBox({
+				"prompt": prompt
+			}).then((input) => {
+				cc(hval(input));
+			}, (err) => {
+				errorMessage("input-string failed with " + err.toString());
+			});
+		}, "input-string");
+
+		// (insert [expression])
+		interp.importCCFunction((args, env, cc) => {
+			var arg = first(args);
+			var toInsert = HT.toMessage(arg);
+			if (vscode.window.activeTextEditor) {
+				vscode.window.activeTextEditor.edit((edit) => {
+					edit.replace(vscode.window.activeTextEditor.selection, toInsert);
+					cc(hval(arg));
+				});
+			} else {
+				errorMessage("You need to select an editor before inserting.");
+			}
+
+		}, "insert");
+
+		// (vscode-command [command name])
+		interp.importCCFunction((args, env, cc) => {
+			commands.executeCommand(string(first(args))).then((result) => {
+				cc(hval(result));
+			}, (err) => {
+				errorMessage(err.toString());
+			});
+		}, "vscode-command");
+
+		// Load the user's launch script
+		var config = vscode.workspace.getConfiguration('hiss-vscode');
+
+		var launchScript = config.get("launchScript");
+		if (launchScript != null) {
+			interp.load(launchScript);
 		}
 
-	}, "insert");
-
-	// Load the user's launch script
-	var config = vscode.workspace.getConfiguration('hiss-vscode');
-
-	var launchScript = config.get("launchScript");
-	if (launchScript != null) {
-		interp.load(launchScript);
-	}
-
-	// Run hiss with continuations enabled to handle async
-	runHiss("(enable-continuations)");
+		// Run hiss with continuations enabled to handle async
+		resolve(runHissExp("(enable-continuations)"));
+	});
 }
 
-function runHiss(code) {
-	interp.eval(interp.read(code));
+/**
+ * 
+ * @param {String} code Hiss code to evaluate
+ */
+function runHissExp(code) {
+	return new Promise ( (resolve, reject) => {
+		interp.evalCC(interp.read(code), (result) => {
+			resolve(value(result, interp));
+		});
+	});
 }
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-	freshInterpreter();
-
 	// Display the value of a Hiss expression
-	let eval = vscode.commands.registerCommand('hiss-vscode.eval', function () {
-		runHiss("(print (input-expression \"Expression to evaluate:\"))");
+	let eval = commands.registerCommand('hiss-vscode.eval', () => {
+		return runHissExp("(print (input-expression \"Expression to evaluate:\"))");
 	});
 
 	// Insert the value of a Hiss expression
-	let insert = vscode.commands.registerCommand('hiss-vscode.insert', function () {		
-		runHiss("(insert (input-expression \"Expression to insert:\"))");
+	let insert = commands.registerCommand('hiss-vscode.insert', () => {		
+		return runHissExp("(insert (input-expression \"Expression to insert:\"))");
 	});
 
 	// Restart the interpreter to clear state
-	let restart = vscode.commands.registerCommand('hiss-vscode.restart', freshInterpreter);
+	let restart = commands.registerCommand('hiss-vscode.restart', freshInterpreter);
 
 	context.subscriptions.push(eval);
 	context.subscriptions.push(insert);
 	context.subscriptions.push(restart);
+
+	freshInterpreter().then(() => {
+		infoMessage("Hiss-vscode is ready.");
+	});
 }
 exports.activate = activate;
 
